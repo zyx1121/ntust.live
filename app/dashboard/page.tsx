1f86fff7d67c8dca746868373f5fac2b68414998
zyx1121 <yongxiang.zhan@outlook.com>
2023-11-06 21:37:51 +0800

    update

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <main className="flex justify-center items-center h-full">
+      <div className="grid gap-4">
+
+      </div>
+    </main>
+  )
+}
