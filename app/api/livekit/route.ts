b64b7b12248b7f39016bc57abd313e7ecabb35c9
zyx1121 <yongxiang.zhan@outlook.com>
2023-12-03 12:38:27 +0800

    update

--- app/api/livekit/route.ts
+++ app/api/livekit/route.ts
@@ -1,43 +1,44 @@
-import { AccessToken } from "livekit-server-sdk";
-import { NextRequest, NextResponse } from "next/server";
+import { AccessToken } from "livekit-server-sdk"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest) {
-  const room = req.nextUrl.searchParams.get("room");
-  const username = req.nextUrl.searchParams.get("username");
-  const id = req.nextUrl.searchParams.get("id");
+  const room = req.nextUrl.searchParams.get("room")
+  const username = req.nextUrl.searchParams.get("username")
+  const id = req.nextUrl.searchParams.get("id")
+
   if (!room) {
     return NextResponse.json(
-      { error: 'Missing "room" query parameter' },
+      { error: "Missing room query parameter" },
       { status: 400 }
-    );
+    )
   }
   if (!username) {
     return NextResponse.json(
-      { error: 'Missing "username" query parameter' },
+      { error: "Missing username query parameter" },
       { status: 400 }
-    );
+    )
   }
   if (!id) {
     return NextResponse.json(
-      { error: 'Missing "id" query parameter' },
+      { error: "Missing id query parameter" },
       { status: 400 }
-    );
+    )
   }
 
-  const apiKey = process.env.LIVEKIT_API_KEY;
-  const apiSecret = process.env.LIVEKIT_API_SECRET;
-  const wsUrl = process.env.NEXT_PUBLIC_LIVEKIT_URL;
+  const apiKey = process.env.LIVEKIT_API_KEY
+  const apiSecret = process.env.LIVEKIT_API_SECRET
+  const wsUrl = process.env.NEXT_PUBLIC_LIVEKIT_URL
 
   if (!apiKey || !apiSecret || !wsUrl) {
     return NextResponse.json(
       { error: "Server misconfigured" },
       { status: 500 }
-    );
+    )
   }
 
-  const at = new AccessToken(apiKey, apiSecret, { name: username, identity: id });
+  const at = new AccessToken(apiKey, apiSecret, { name: username, identity: id })
 
-  at.addGrant({ room, roomJoin: true, canPublish: true, canSubscribe: true });
+  at.addGrant({ room, roomJoin: true, canPublish: true, canSubscribe: true })
 
-  return NextResponse.json({ token: at.toJwt() });
+  return NextResponse.json({ token: at.toJwt() })
 }
