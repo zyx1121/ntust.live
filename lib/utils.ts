14aff35e0a27fe3d6ef097b0355b9bce53c01733
zyx1121 <yongxiang.zhan@outlook.com>
2023-12-10 15:33:09 +0800

    update

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+import { getSelectorsByUserAgent } from "react-device-detect";
+
+export const useMobile = () => {
+  if (typeof navigator === "undefined") return false;
+  const { isMobile } = getSelectorsByUserAgent(navigator.userAgent);
+  return isMobile;
+};
