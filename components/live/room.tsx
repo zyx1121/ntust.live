8ab14b674da17c718427cd5884c4b39645bc8746
zyx1121 <yongxiang.zhan@outlook.com>
2023-12-10 14:54:11 +0800

    update

--- components/live/room.tsx
+++ components/live/room.tsx
@@ -1,7 +1,9 @@
 import { GridLayout, RoomAudioRenderer, useLocalParticipant, useParticipants, useTracks } from "@livekit/components-react"
 import { User } from "@prisma/client"
 import { RoomEvent, Track } from "livekit-client"
+import { useState } from "react"
 import { Badge } from "../ui/badge"
+import { Slider } from "../ui/slider"
 import { ControlBar } from "./bar"
 import { Chat } from "./chat"
 import { ParticipantTile } from "./participan"
@@ -23,6 +25,8 @@ export function Room({ room, users, authenticated }: { room: string, users: User
     (track) => track.participant?.identity == streamer || linkers?.includes(track.participant?.identity)
   )
 
+  const [sliderValue, setSliderValue] = useState(0.5)
+
   return (
     <div className="relative flex items-stretch h-full p-4 gap-4 bg-background">
       <div className="flex flex-col items-stretch w-full lg:h-[calc(100dvh-5.5rem-1px)] h-[calc(100dvh-9rem-1px)] border rounded-md">
@@ -39,13 +43,17 @@ export function Room({ room, users, authenticated }: { room: string, users: User
           <Badge variant="secondary" className="absolute top-24 left-12">
             在線 {participants.length}
           </Badge>
+          <Slider className="absolute top-[6.5rem] left-32 w-32" defaultValue={[0.5]} max={1} step={0.1} onValueChange={(v) => setSliderValue(v.at(0)!)} />
         </>
       ) : (
-        <Badge variant="secondary" className="absolute top-12 left-12">
-          在線 {participants.length}
-        </Badge>
+        <>
+          <Badge variant="secondary" className="absolute top-12 left-12">
+            在線 {participants.length}
+          </Badge>
+          <Slider className="absolute top-14 left-32 w-32" defaultValue={[0.5]} max={1} step={0.1} onValueChange={(v) => setSliderValue(v.at(0)!)} />
+        </>
       )}
-      <RoomAudioRenderer />
+      <RoomAudioRenderer volume={sliderValue} />
     </div>
   )
 }
